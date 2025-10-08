55c2b909e05f0e351c837a87000b227663a471ad
MSadawi <sadawe147@gmail.com>
2025-10-08 17:47:29 +0800

    Learn basic data types and printing

--- day1/Main.java
+++ day1/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Argument" + " " + (i + 1) + ": " + args[i]);
-        }
         String name = "Sadawi";
-        int age = 19;
-        float height = 1.72f;
-        char initial = 'M';
-        final boolean isAdult = true;
-        isAdult = false;
-        int integer = isAdult ? 1 : 0;
-        System.out.println(integer);
+        char gender = 'M';
+        int year = 2025;
+        // print information
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Year: " + 2025);
+        // print information on same line
+        System.out.print("Name: " + name);
+        System.out.print("Gender: " + gender);
+        System.out.print("Year: " + year);
+        System.out.println();
+        // print errors
+        System.err.println("Error: learning is taking so long");
     }
 }
