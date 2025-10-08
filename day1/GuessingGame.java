fdd9a35c5d4938e16dc6a4c55a102ad61f09d1de
MSadawi <sadawe147@gmail.com>
2025-10-08 20:53:14 +0800

    Create a guessing game to apply day 1 fundamentals

--- day1/GuessingGame.java
+++ day1/GuessingGame.java
@@ -2,16 +2,43 @@ import java.util.Scanner;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Word Guesser Game!");
 
-        String secret = "java";
-        Scanner scanner = new Scanner(System.in);
+        boolean isPlaying = true;
 
-        System.out.print("You have 5 attempts to guess the secret word");
-        for (int i = 0; i < secret.lengnt(); i++) {
-            System.out.print("_");
+        while (isPlaying) {
+            System.out.println("Welcome to the Word Guesser Game!");
+
+            String secret = "java";
+            Scanner scanner = new Scanner(System.in);
+
+            int attempts = 5;
+            System.out.print("You have 5 attempts to guess the secret word: ");
+
+            for (int i = 0; i < secret.length(); i++) {
+                System.out.print("_");
+            }
+
+            while (attempts > 0) {
+                System.out.println();
+                String guess = scanner.nextLine();
+                if (secret.equalsIgnoreCase(guess)) {
+                    System.out.println("Correct! Enter [p] to continue!");
+                    isPlaying = false;
+                    break;
+                } else {
+                    attempts--;
+                    System.out.println("Incorrect! You have " + attempts + " attempts remaining.");
+                }
+            }
+            if (attempts == 0) {
+                System.out.println("No attempts left. Game Over!");
+                System.out.println("Ender [p] to continue!");
+                isPlaying = false;
+            }
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("p")) {
+                isPlaying = true;
+            }
         }
-        System.out.println();
-        String guess = scanner.nextLine();
     }
 }
