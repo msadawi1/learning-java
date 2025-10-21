68da3a392c27ee880a749bc155ceb92f85066bd6
MSadawi <sadawe147@gmail.com>
2025-10-21 19:05:23 +0800

    Finish calculator logic

--- day4-capstone-project-Java-GUI/CalculatorGUI.java
+++ day4-capstone-project-Java-GUI/CalculatorGUI.java
@@ -8,7 +8,9 @@ public class CalculatorGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        Font font = new Font("SansSerif", Font.BOLD, 16);
+        Font font = new Font("SansSerif", Font.BOLD, 20);
+        Font fieldFont = new Font("SansSerif", Font.PLAIN, 18);
+
 
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
@@ -36,12 +38,18 @@ public class CalculatorGUI {
 
         var firstNumField = new JTextField();
         firstNumField.setPreferredSize(new Dimension(100, 30));
+        firstNumField.setFont(fieldFont);
+        firstNumField.setHorizontalAlignment(JTextField.CENTER);
 
         var opField = new JTextField();
         opField.setPreferredSize(new Dimension(100, 30));
+        opField.setFont(fieldFont);
+        opField.setHorizontalAlignment(JTextField.CENTER);
 
         var secondNumField = new JTextField();
         secondNumField.setPreferredSize(new Dimension(100, 30));
+        secondNumField.setFont(fieldFont);
+        secondNumField.setHorizontalAlignment(JTextField.CENTER);
 
         centerPanel.add(firstNumField);
         centerPanel.add(opField);
@@ -64,9 +72,64 @@ public class CalculatorGUI {
 
         bottomPanel.add(resultLabel);
         JTextField resultField = new JTextField();
+        resultField.setFont(fieldFont);
         resultField.setPreferredSize(new Dimension(100, 30));
+        resultField.setHorizontalAlignment(JTextField.CENTER);
 
         bottomPanel.add(resultField);
+
+        // calculator logic
+        calculateButton.addActionListener(input -> {
+            double firstNum;
+            double secondNum;
+            double result = Double.NaN;
+
+            String firstNumString = firstNumField.getText();
+            firstNumString.trim();
+            if (firstNumString.isEmpty()) {
+                resultField.setText("Error: first number empty");
+                return;
+            }
+
+            String secondNumString = secondNumField.getText();
+            secondNumString.trim();
+            if (secondNumString.isEmpty()) {
+                resultField.setText("Error: second number empty");
+                return;
+            }
+
+
+            try {
+                firstNum = Double.parseDouble(firstNumString);
+                secondNum = Double.parseDouble(secondNumString);
+            } catch (NumberFormatException e) {
+                resultField.setText("Please enter numbers only.");
+                return;
+            }
+
+            String operation = opField.getText();
+            operation.trim();
+            if (operation.isEmpty()) {
+                resultField.setText("Error: operation empty");
+                return;
+            }
+
+
+            if (operation.equals("/") || operation.equals("÷")) {
+                result = firstNum / secondNum;
+            } else if (operation.equals("*") || operation.equals("×")) {
+                result = firstNum * secondNum;
+            } else if (operation.equals("+")) {
+                result = firstNum + secondNum;
+            } else if (operation.equals("-")) {
+                result = firstNum - secondNum;
+            }
+            if (Double.isNaN(result)) {
+                resultField.setText("Please choose a supported operation.");
+            } else {
+                resultField.setText("" + result);
+            }
+        });
 
         frame.add(bottomPanel, BorderLayout.SOUTH);
         frame.add(topPanel, BorderLayout.NORTH);
